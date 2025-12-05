c0467a475bf64dd47ecab5a44351799706831447
juan jose <juanjosevelasquezgalvis563@gmail.com>
2025-12-04 20:55:03 -0600

    feat(src): agregar función agregarItem

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-function suma(a, b) {
-  return a + b;
-}
-
 function agregarItem(texto) {
   const lista = document.getElementById('lista');
   const li = document.createElement('li');
@@ -9,6 +5,19 @@ function agregarItem(texto) {
   lista.appendChild(li);
 }
 
+// Evento del botón
+document.getElementById('btnAgregar').addEventListener('click', () => {
+  const input = document.getElementById('inputTexto');
+  const texto = input.value.trim();
+
+  if (texto === "") {
+    alert("Escribe algo para agregar");
+    return;
+  }
+
+  agregarItem(texto);
+  input.value = "";
+});
+
 console.log("App lista");
 
-module.exports = { suma, agregarItem };
