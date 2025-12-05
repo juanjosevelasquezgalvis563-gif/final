aff88fc50a744b642ba62770c7f24de4753870b7
juan jose <juanjosevelasquezgalvis563@gmail.com>
2025-12-04 20:51:09 -0600

    feat(ui): agregar lista y botón

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ function suma(a, b) {
   return a + b;
 }
 
+function agregarItem(texto) {
+  const lista = document.getElementById('lista');
+  const li = document.createElement('li');
+  li.textContent = texto;
+  lista.appendChild(li);
+}
+
 console.log("App lista");
 
-module.exports = { suma };
+module.exports = { suma, agregarItem };
